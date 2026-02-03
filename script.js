2687bca08b47e92750d35d74d30cf6998eb0a7c7
T.A.Niranjhan Ragul <niranjhanraghul@gmail.com>
2026-02-03 10:28:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// Step 19: JavaScript Interactivity
+
+function changeBackground() {
+  document.body.style.backgroundColor = "lightgreen";
+}
+
+function animateHero() {
+  const heroText = document.querySelector(".hero h1");
+  heroText.style.color = "red";
+  heroText.style.transition = "color 1s ease";
+}
+
+function handleForm(event) {
+  event.preventDefault();
+  alert("Thanks for your message!");
+}
 document.addEventListener("DOMContentLoaded", function() {
   alert("Welcome to my site!");
 });
